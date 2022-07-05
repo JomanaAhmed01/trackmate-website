c80f1255c16daf60bb743a09530fc06fd0cfa38e
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-05 22:12:47 +0200

    Defining WorkOutTrainingHeader.js file in the HomePageCompound.js file

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -2,17 +2,26 @@ import React from 'react';
 import styled from "styled-components"
 import Navbar from '../components/HomePage/Navbar'
 import DailyRoutineHeader from '../components/HomePage/DailyRoutineHeader'
+import WorkOutTrainingHeader from '../components/HomePage/WorkOutTrainingHeader'
 
 function HomePageCompound() {
   return (
     <Wrapper>
-      <Navbar />
-      <DailyRoutineHeader />
+      <HeaderWrapper>
+        <Navbar />
+        <DailyRoutineHeader />
+      </HeaderWrapper>
+
+      <WorkOutTrainingHeader />
     </Wrapper>
   );
 }
 
 export const Wrapper = styled.div`
+  
+`
+
+export const HeaderWrapper = styled.div`
   background-image: url('./img/bg.png');
   width: 1349px;
   height: 1081px;
