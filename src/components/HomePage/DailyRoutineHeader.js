635892b76f8e86df0e3280a30e23e0af2180a8b4
Jomana Ahmed <ojy.ahmed@outlook.com>
2022-07-05 16:24:53 +0200

    Creating DailyRoutineHeader.js file

---
+++ src/components/HomePage/DailyRoutineHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from "styled-components"
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500;700&family=Poppins&family=Quicksand:wght@300&family=Roboto:wght@300&family=Suez+One&display=swap');
+</style>
+
+function DailyRoutineHeader() {
+  return (
+    <Wrapper>
+      sssssssssssssssss
+    </Wrapper>
+  );
+}
+
+
+export const Wrapper = styled.div`
+  
+`
+
+export default DailyRoutineHeader;
